2c0735705352067f9e399818d97a26689955e68e
= <=>
2024-05-02 00:31:15 -0500

    Cambio de repo

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.export = function(grunt){
+  grunt.initConfig({
+    sass: {
+        dist:{
+            files: [{
+                expand: true,
+                cwd:'css',
+                src: ['*.scss'],
+                dest: 'css',
+                ext: '.css'
+            }]
+        }
+    },
+    watch: {
+        files: ['css/*.scss'],
+        tasks: ['css']
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('css',['sass']);
+};
